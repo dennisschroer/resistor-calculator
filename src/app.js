22af435140aa36b4796e9065b9aad94ed34720af
Dennis Schroer <dev@dennisschroer.nl>
2017-09-16 12:01:03 +0200

    Use webpack too for less and completely remove the dependency on grunt

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 
 import MainController from './controllers/main';
+import NumberFormatFilter from './filters/number-format';
 
-import NumberFormatFilter from './filters/number-format'
+import './app.less';
 
 angular.module('resistorCalculator', ['ngMaterial'])
   .controller('MainController', MainController)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: {
     vendor: ['angular', 'angular-animate', 'angular-aria', 'angular-material'],
     app: './src/app.js'
   },
+  devtool: 'source-map',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
@@ -18,6 +20,21 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor",
       minChunks: Infinity
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
     })
   ],
+  module: {
+    rules: [{
+      test: /\.less$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "less-loader" // compiles Less to CSS
+      }]
+    }]
+  }
 };

0a76d2ac4c0f29a9dd73f1074a4c97f7f2f6c3ce
Dennis Schroer <dev@dennisschroer.nl>
2017-09-16 16:53:46 +0200

    Add preview of resistor

---
+++ src/constants/bands.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    value: 0,
+    label: 0,
+    color: 'black'
+  },
+  {
+    value: 1,
+    label: 1,
+    color: 'brown'
+  },
+  {
+    value: 2,
+    label: 2,
+    color: 'red'
+  },
+  {
+    value: 3,
+    label: 3,
+    color: 'orange'
+  },
+  {
+    value: 4,
+    label: 4,
+    color: 'yellow'
+  },
+  {
+    value: 5,
+    label: 5,
+    color: 'green'
+  },
+  {
+    value: 6,
+    label: 6,
+    color: 'blue'
+  },
+  {
+    value: 7,
+    label: 7,
+    color: 'violet'
+  },
+  {
+    value: 8,
+    label: 8,
+    color: 'grey'
+  },
+  {
+    value: 9,
+    label: 9,
+    color: 'white'
+  }
+];
